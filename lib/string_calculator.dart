ce24770a99e6e4b1551fdad5ca7f3ea169e7c9c5
Raghav Yadlapalli <Raghav.Yadlapalli@nfstechgroup.com>
2025-07-22 22:52:06 +0530

    refactor conditions

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,12 +1,8 @@
 class StringCalculator {
   int add(String numbers) {
-    if (numbers.isEmpty) {
-      return 0;
-    }
-    if (numbers.contains(',')) {
-      final numbersList = numbers.split(',');
-      return int.parse(numbersList[0]) + int.parse(numbersList[1]);
-    }
-    return int.parse(numbers);
+    if (numbers.isEmpty) return 0;
+    if (!numbers.contains(',')) return int.parse(numbers);
+    final numbersList = numbers.split(',');
+    return int.parse(numbersList[0]) + int.parse(numbersList[1]);
   }
 }
